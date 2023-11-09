85042390e5687a68affcf2a6fc6807ee273d32a7
silentm0de <rytis@wisemonks.com>
2023-11-09 09:50:05 +0200

    Update schedule.rb to include job_types, env_path and output

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,14 @@
 
 # Example:
 #
-# set :output, "/path/to/my/cron_log.log"
+# set :output, "./log/cron.log"
+set :output, "./log/cron.log"
+set :env_path, '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
+
+job_type :rails,  %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec rails :task :output }
+job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
+job_type :script, %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec bin/:task :output }
+
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
@@ -22,5 +29,5 @@
 every '0 9 * * 1-5', roles: [:whenever] do
   # runner "Leaderboard.calculate_leaderboard_for_admins"
   # runner "Leaderboard.calculate_leaderboard_for_each_user"
-  rake "leaderboards:send_leaderboards"
+  rails "leaderboards:send_leaderboards"
 end
