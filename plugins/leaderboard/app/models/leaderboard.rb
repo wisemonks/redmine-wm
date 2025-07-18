d6c70bd29b1b4f42cea63d7b393c62d5542fe8c1
silentm0de <rytis@wisemonks.com>
2025-07-18 13:05:15 +0300

    Exclude managers from spent time reminders

--- plugins/leaderboard/app/models/leaderboard.rb
+++ plugins/leaderboard/app/models/leaderboard.rb
@@ -78,7 +78,7 @@ class Leaderboard < ActiveRecord::Base
   end
 
   def self.send_reminders
-    ['arturas', 'rytis', 'rokas', 'raminta', 'agneta', 'edvinas', 'adomas', 'viktorija'].each do |user|
+    ['rokas', 'raminta', 'agneta', 'edvinas', 'adomas', 'viktorija'].each do |user|
       Mattermost::Base.new.post_message(user, Leaderboard.mattermost_reminder_message)
     end
   end
