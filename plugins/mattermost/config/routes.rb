d827b6e07ec8d33608c587c4330fda3581763792
silentm0de <rytis@wisemonks.com>
2025-08-19 16:54:45 +0300

    Different routes config

--- plugins/mattermost/config/routes.rb
+++ plugins/mattermost/config/routes.rb
@@ -1,8 +1,10 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get 'tasks', to: 'tasks#index'
-get 'review', to: 'tasks#review'
-get 'resolve', to: 'tasks#resolve'
-get 'finish', to: 'tasks#finish'
-post 'spent', to: 'tasks#spent'
+# get 'tasks', to: 'tasks#index'
+# get 'review', to: 'tasks#review'
+# get 'resolve', to: 'tasks#resolve'
+# get 'finish', to: 'tasks#finish'
+# post 'spent', to: 'tasks#spent'
+
+resources :tasks, only: [:index, :review, :resolve, :finish, :spent]
