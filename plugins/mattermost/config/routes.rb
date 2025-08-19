f3fba46406b7087c95b681f0fd246d8ddb5f10ed
silentm0de <rytis@wisemonks.com>
2025-08-19 18:00:00 +0300

    Routes change

--- plugins/mattermost/config/routes.rb
+++ plugins/mattermost/config/routes.rb
@@ -1,17 +1,24 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-# get 'tasks', to: 'tasks#index'
+# get 'tasks', to: 'tasks#index', as: 'tasks'
+# point /tasks GET request to plugins/mattermost/app/controllers/tasks_controller.rb
+scope :mattermost do
+  get 'tasks', to: 'tasks#index', as: 'mattermost_tasks'
+end
+
+
+
 # get 'review', to: 'tasks#review'
 # get 'resolve', to: 'tasks#resolve'
 # get 'finish', to: 'tasks#finish'
 # post 'spent', to: 'tasks#spent'
 
-resources :tasks, only: [:index] do
-  member do
-    post :review
-    post :resolve
-    post :finish
-    post :spent
-  end
-end
+# resources :tasks, only: [:index] do
+#   member do
+#     post :review
+#     post :resolve
+#     post :finish
+#     post :spent
+#   end
+# end
