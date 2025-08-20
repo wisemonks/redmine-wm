8215a3fb334aff9067c1b961e37f01e6bc61fa7d
silentm0de <rytis@wisemonks.com>
2025-08-20 12:14:54 +0300

    Open more redmine-mattermost routes

--- plugins/mattermost/config/routes.rb
+++ plugins/mattermost/config/routes.rb
@@ -5,6 +5,10 @@
 # point /tasks GET request to plugins/mattermost/app/controllers/tasks_controller.rb
 scope :mattermost do
   get 'tasks', to: 'tasks#index', as: 'mattermost_tasks'
+  get 'review', to: 'tasks#review', as: 'mattermost_review'
+  get 'resolve', to: 'tasks#resolve', as: 'mattermost_resolve'
+  get 'finish', to: 'tasks#finish', as: 'mattermost_finish'
+  post 'spent', to: 'tasks#spent', as: 'mattermost_spent'
 end
 
 
