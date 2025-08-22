942e1018eb6b13d74c69764627baef1bd1fe7ad6
silentm0de <rytis@wisemonks.com>
2025-08-22 14:36:29 +0300

    Fix migration

--- plugins/mattermost/db/migrate/20250822105012_add_matermost_fields.rb
+++ plugins/mattermost/db/migrate/20250822105012_add_matermost_fields.rb
@@ -3,12 +3,12 @@ class AddMatermostFields < ActiveRecord::Migration[6.1]
     add_column :users, :mattermost_user_id, :string
 
     User.find(134).update(mattermost_user_id: 'sckbn6jws3dx9bxfz4dizfwgbo')
-    User.find(151).update(mattermost_user_id: 'dcm5ywcw5jbkmxece4dnngex1r')
-    User.find(156).update(mattermost_user_id: 'store77e87g7zjuxi3ieg31roy')
-    User.find(161).update(mattermost_user_id: 'pabetp7expfrdniyy66a654c1e')
-    User.find(172).update(mattermost_user_id: 'mfh6rnigmiy7zrptoeioytfc8c')
-    User.find(1).update(mattermost_user_id: '8bobf9fiqbryfrkr6ciz3rm5ww')
-    User.find(169).update(mattermost_user_id: 'jxddbgtw6tntfcoah84uiap5ne')
-    User.find(178).update(mattermost_user_id: 'bh8ojm7jmffomxrm84gbphruah')
+    User.find(151).update(mattermost_user_id: 's1btbbe6fbg3xf7p8daay46osh') 
+    User.find(156).update(mattermost_user_id: 'o7ix8s8egirepm5kcm7umsm96e')
+    User.find(161).update(mattermost_user_id: 'gd4xp7mb83yzfgs9niyyxk3tey')
+    User.find(172).update(mattermost_user_id: '5rsb63p1pidxfe8dffi8n3etay')
+    User.find(1).update(mattermost_user_id: 'u5xkkzeuh7dn7gzztugi5b5u7e')
+    User.find(169).update(mattermost_user_id: 'emr15brb1trsp8xbaioijwgt3r')
+    User.find(178).update(mattermost_user_id: 'ixmmjqhn73duzmna5jmtfhwhdh')
   end
 end
